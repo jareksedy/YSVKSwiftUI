0a94931f3cc0cd9cf1e201ed957fdaa3f532485b
Jarek <jareksedy@icloud.com>
2021-10-21 21:07:09 +0600

    Initial Commit

---
+++ YSVKSwiftUI/YSVKSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YSVKSwiftUIApp.swift
+//  YSVKSwiftUI
+//
+//  Created by Ярослав on 21.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct YSVKSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
