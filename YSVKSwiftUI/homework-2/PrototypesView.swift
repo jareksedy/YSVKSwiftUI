3753c3d9ddd3be877157da43bfa8417107547e31
Jarek <jareksedy@icloud.com>
2021-10-23 21:27:37 +0600

    Added PrototypesView.swift

---
+++ YSVKSwiftUI/homework-2/PrototypesView.swift
@@ -0,0 +1,20 @@
+//
+//  PrototypesView.swift
+//  YSVKSwiftUI
+//
+//  Created by Ярослав on 23.10.2021.
+//
+
+import SwiftUI
+
+struct PrototypesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrototypesView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrototypesView()
+    }
+}
