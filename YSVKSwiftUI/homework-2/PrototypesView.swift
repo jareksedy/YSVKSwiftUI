a25b0261ea70bb01c69df29b0aa56f601f4f10ce
Jarek <jareksedy@icloud.com>
2021-10-23 21:38:18 +0600

    Added PlainList view modifier.

--- YSVKSwiftUI/homework-2/PrototypesView.swift
+++ YSVKSwiftUI/homework-2/PrototypesView.swift
@@ -9,12 +9,24 @@ import SwiftUI
 
 struct PrototypesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Text("Hi there").padding()
+            Text("Holy shit!").padding()
+        }.modifier(PlainList())
     }
 }
 
 struct PrototypesView_Previews: PreviewProvider {
     static var previews: some View {
         PrototypesView()
+    }
+}
+
+// MARK: - View modifiers.
+
+struct PlainList: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .listStyle(.plain)
     }
 }
