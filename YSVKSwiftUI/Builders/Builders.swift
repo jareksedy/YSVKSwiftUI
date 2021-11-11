c34f8143392020e9470ed2add2776ae91a514440
Jarek Šedý <jareksedy@icloud.com>
2021-11-11 20:36:57 +0600

    Touch spring animation.

--- YSVKSwiftUI/Builders/Builders.swift
+++ YSVKSwiftUI/Builders/Builders.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import Kingfisher
 
 struct AvatarImage: View {
+    @State private var scaleFactor = 1.0
     var content: KFImage
  
     init(@ViewBuilder content: () -> KFImage) {
@@ -21,5 +22,11 @@ struct AvatarImage: View {
             .aspectRatio(contentMode: .fit)
             .frame(width: 100, height: 100)
             .clipShape(Circle())
+            .scaleEffect(scaleFactor)
+            .animation(.spring(response: 0.2, dampingFraction: 0.35, blendDuration: 0.25))
+            .onTouchGesture(
+                touchBegan: { withAnimation { self.scaleFactor = 1.75 } },
+                touchEnd: { _ in withAnimation { self.scaleFactor = 1.0 } }
+            )
     }
 }
