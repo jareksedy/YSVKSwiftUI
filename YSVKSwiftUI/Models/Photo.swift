5f8e00ce241d9d8159dd9bafd46beaca2348008b
Jarek Šedý <jareksedy@icloud.com>
2021-11-20 13:26:05 +0600

    Added Photo data model.

---
+++ YSVKSwiftUI/Models/Photo.swift
@@ -0,0 +1,64 @@
+//
+//  Photo.swift
+//  ys-client-server-1347
+//
+//  Created by Ярослав on 13.07.2021.
+//
+
+
+import Foundation
+
+// MARK: - Feed
+
+struct Photos: Codable {
+    let response: PhotoResponse
+}
+
+// MARK: - Response
+
+struct PhotoResponse: Codable {
+    let count: Int
+    let items: [PhotoItem]?
+}
+
+// MARK: - Item
+
+struct PhotoItem: Codable {
+    let albumID, date, id, ownerID: Int?
+    let hasTags: Bool?
+    let sizes: [Size]?
+    let text: String?
+    let lat, long: Double?
+    let postID: Int?
+    
+    var photoAvailable: Size? {
+        
+        guard let sizes = self.sizes else { return nil }
+        if let photo = sizes.first(where: {$0.type == "x"}) { return photo }
+        if let photo = sizes.first(where: {$0.type == "z"}) { return photo }
+        if let photo = sizes.first(where: {$0.type == "y"}) { return photo }
+        if let photo = sizes.first(where: {$0.type == "m"}) { return photo }
+        if let photo = sizes.first(where: {$0.type == "s"}) { return photo }
+        
+        return sizes.first
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case albumID = "album_id"
+        case date, id
+        case ownerID = "owner_id"
+        case hasTags = "has_tags"
+        case sizes, text, lat, long
+        case postID = "post_id"
+    }
+}
+
+
+// MARK: - Size
+
+struct Size: Codable {
+    let height: Int
+    let url: String
+    let type: String
+    let width: Int
+}
